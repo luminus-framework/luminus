2b88398eeb18609b215189c1c32bde865abe026c
Yogthos <dmitri.sotnikov@gmail.com>
2013-04-01 11:27:29 -0400

    removed hiccup dependency, already provided by lib-noir

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [lib-noir "0.4.9"]
-                 [compojure "1.1.5"]
-                 [hiccup "1.0.2"]                 
+                 [compojure "1.1.5"]                                 
                  [ring-server "0.2.7"]                 
                  [markdown-clj "0.9.19"]
                  [clj-http "0.6.3"]
