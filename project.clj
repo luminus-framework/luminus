e6ea871cb0ee4da37c315a020854a8bf83a70e91
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2013-12-03 20:05:32 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject luminus "0.1.0-SNAPSHOT"
   :description "Documentation site for the Luminus framework"
-  :url "http://example.com/FIXME"
+  :url "http://www.luminusweb.net/"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [lib-noir "0.6.4"]
                  [compojure "1.1.5"]                                 
