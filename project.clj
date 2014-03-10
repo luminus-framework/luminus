4dddde7c60672b19ebb652ec595e32454243cf79
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2014-03-10 12:45:42 -0400

    updated dependencies

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [lib-noir "0.8.0"]
+                 [lib-noir "0.8.1"]
                  [compojure "1.1.6"]
                  [ring-server "0.3.1"]
                  [markdown-clj "0.9.41"]
-                 [clj-http "0.7.9"]
+                 [clj-http "0.9.0"]
                  [crouton "0.1.1"]]
   :min-lein-version "2.0.0"
   :plugins [[lein-ring "0.8.10"]]
