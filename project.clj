e1c72983d3945e0853a697296651ee4a77ce66d5
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2014-08-18 13:15:34 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [compojure "1.1.8"]
                  [ring-server "0.3.1"]
                  [markdown-clj "0.9.47"]
-                 [clj-http "0.9.2"]
+                 [clj-http "1.0.0"]
                  [crouton "0.1.2"]]
   :min-lein-version "2.0.0"
   :plugins [[lein-ring "0.8.10"]]
