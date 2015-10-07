2a84f06c7f022aa0060dbe9d3072b51805bbe46c
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-10-06 21:13:04 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -5,12 +5,12 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.cache "0.6.4"]
-                 [compojure "1.3.4"]
-                 [ring-server "0.3.1"]
-                 [markdown-clj "0.9.69"]
-                 [clj-http "1.1.2"]
+                 [compojure "1.4.0"]
+                 [ring-server "0.4.0"]
+                 [markdown-clj "0.9.74"]
+                 [clj-http "2.0.0"]
                  [crouton "0.1.2"]
-                 [selmer "0.8.2"]]
+                 [selmer "0.9.2"]]
   :min-lein-version "2.0.0"
   :plugins [[lein-ring "0.9.6"]]
   :profiles {:production
