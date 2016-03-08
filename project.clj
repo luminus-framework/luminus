bfd24712ff325fc0d4138805250724ad3168f507
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-03-08 08:27:15 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "http://www.luminusweb.net/"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.cache "0.6.4"]
-                 [compojure "1.4.0"]
+                 [compojure "1.45.0"]
                  [ring-server "0.4.0"]
-                 [markdown-clj "0.9.76"]
-                 [clj-http "2.0.0"]
+                 [markdown-clj "0.9.86"]
+                 [clj-http "2.1.0"]
                  [crouton "0.1.2"]
-                 [selmer "0.9.2"]]
+                 [selmer "1.0.2"]]
   :min-lein-version "2.0.0"
   :plugins [[lein-ring "0.9.7"]]
   :profiles {:production
